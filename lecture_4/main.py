717f7c829a2297bbb783b8659f2dd7edb346d25a
X-Oleg <Oleg.Kharishin@gmail.com>
2022-10-16 18:16:42 +0300

    Python_lecture_work-004

--- lecture_4/main.py
+++ lecture_4/main.py
@@ -1,6 +1,6 @@
 import html_creater as hc
 import data_provider as dp
 
-# print(hc.create()) # ver 1
+print(hc.create()) # ver 1
 
-hc.create(dp.data_collect())
+# hc.create(dp.data_collect()) # ver 2 - not work write CSV
